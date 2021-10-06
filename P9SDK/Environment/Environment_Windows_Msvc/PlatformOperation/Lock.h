a4a687a044a21e9f749af8403b88c749990a52bf
dexterdreeeam <dexterdreeeam@foxmail.com>
2021-10-06 21:29:55 +0800

    add rwlock

--- P9SDK/Environment/Environment_Windows_Msvc/PlatformOperation/Lock.h
+++ P9SDK/Environment/Environment_Windows_Msvc/PlatformOperation/Lock.h
@@ -3,10 +3,25 @@
 typedef  hndl  lock;
 
 _INLINE_ lock  lock_create(void);
-_INLINE_ RET   lock_try_get(lock lk);
+_INLINE_ boole lock_try_get(lock lk);
 _INLINE_ void  lock_wait_get(lock lk);
 _INLINE_ void  lock_put(lock lk);
 _INLINE_ void  lock_destroy(lock lk);
+
+struct rw_lock
+{
+    lock _write;
+    s64  _is_writing;
+    s64  _reader_count;
+};
+
+_INLINE_ rw_lock rw_lock_create(void);
+_INLINE_ boole   rw_lock_try_read(rw_lock lk);
+_INLINE_ boole   rw_lock_wait_read(rw_lock lk);
+_INLINE_ boole   rw_lock_try_write(rw_lock lk);
+_INLINE_ boole   rw_lock_wait_write(rw_lock lk);
+_INLINE_ boole   rw_lock_put_read(rw_lock lk);
+_INLINE_ boole   rw_lock_put_write(rw_lock lk);
 
 _INLINE_ lock lock_create(void)
 {
@@ -15,11 +30,11 @@ _INLINE_ lock lock_create(void)
 
 typedef unsigned long DWORD;
 
-_INLINE_ RET lock_try_get(lock lk)
+_INLINE_ boole lock_try_get(lock lk)
 {
     assert(lk);
     return
-        WindowsMsvcNs::WaitForSingleObject((WindowsMsvcNs::HANDLE)lk, 0) == WAIT_OBJECT_0 ? RET_SUCCESS : RET_FAILED;
+        WindowsMsvcNs::WaitForSingleObject((WindowsMsvcNs::HANDLE)lk, 0) == WAIT_OBJECT_0;
 }
 
 _INLINE_ void lock_wait_get(lock lk)
@@ -38,4 +53,38 @@ _INLINE_ void lock_destroy(lock lk)
 {
     assert(lk);
     WindowsMsvcNs::CloseHandle((WindowsMsvcNs::HANDLE)lk);
+}
+
+_INLINE_ rw_lock rw_lock_create(void)
+{
+    rw_lock lk;
+    lk._write = lock_create();
+    lk._is_writing = 0;
+    lk._reader_count = 0;
+    return lk;
+}
+
+_INLINE_ boole   rw_lock_try_read(rw_lock lk)
+{
+
+}
+
+_INLINE_ boole   rw_lock_wait_read(rw_lock lk)
+{
+}
+
+_INLINE_ boole   rw_lock_try_write(rw_lock lk)
+{
+}
+
+_INLINE_ boole   rw_lock_wait_write(rw_lock lk)
+{
+}
+
+_INLINE_ boole   rw_lock_put_read(rw_lock lk)
+{
+}
+
+_INLINE_ boole   rw_lock_put_write(rw_lock lk)
+{
 }
