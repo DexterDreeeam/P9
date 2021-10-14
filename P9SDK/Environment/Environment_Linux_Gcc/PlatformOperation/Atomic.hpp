2b89f4d83505628d0b7b577b2fa1d6f3ac92feb1
dexterdreeeam <dexterdreeeam@foxmail.com>
2021-10-15 01:36:21 +0800

    add linux atomic

--- P9SDK/Environment/Environment_Linux_Gcc/PlatformOperation/Atomic.hpp
+++ P9SDK/Environment/Environment_Linux_Gcc/PlatformOperation/Atomic.hpp
@@ -22,50 +22,53 @@ _INLINE_ u64 random(u64 mod)
 
 _INLINE_ s32 atom_increment(s32 volatile& x)
 {
-    return LinuxGccNs::InterlockedIncrement(&x);
+    return LinuxGccNs::__atomic_fetch_add(&x, (s32)1, LinuxGccNs::memory_order_relaxed) + 1;
 }
 
 _INLINE_ s64 atom_increment(s64 volatile& x)
 {
-    return LinuxGccNs::InterlockedIncrement64(&x);
+    return LinuxGccNs::__atomic_fetch_add(&x, (s64)1, LinuxGccNs::memory_order_relaxed) + 1;
 }
 
 _INLINE_ s32 atom_decrement(s32 volatile& x)
 {
-    return LinuxGccNs::InterlockedDecrement(&x);
+    return LinuxGccNs::__atomic_fetch_sub(&x, (s32)1, LinuxGccNs::memory_order_relaxed) - 1;
 }
 
 _INLINE_ s64 atom_decrement(s64 volatile& x)
 {
-    return LinuxGccNs::InterlockedDecrement64(&x);
+    return LinuxGccNs::__atomic_fetch_sub(&x, (s64)1, LinuxGccNs::memory_order_relaxed) - 1;
 }
 
 _INLINE_ s32 atom_exchange(s32 volatile& x, s32 replace)
 {
-    return LinuxGccNs::InterlockedExchange(&x, replace);
+    return LinuxGccNs::__atomic_exchange(&x, replace);
 }
 
 _INLINE_ s64 atom_exchange(s64 volatile& x, s64 replace)
 {
-    return LinuxGccNs::InterlockedExchange64(&x, replace);
+    return LinuxGccNs::__atomic_exchange(&x, replace);
 }
 
 _INLINE_ void* atom_exchange(void* volatile& x, void* replace)
 {
-    return LinuxGccNs::InterlockedExchangePointer(&x, replace);
+    return LinuxGccNs::__atomic_exchange(&x, replace);
 }
 
 _INLINE_ s32 atom_compare_exchange(s32 volatile& x, s32 compare, s32 replace)
 {
-    return LinuxGccNs::InterlockedCompareExchange(&x, replace, compare);
+    s32 ret = compare;
+    LinuxGccNs::__atomic_compare_exchange_strong(&x, &ret, replace) ? compare : ret;
 }
 
 _INLINE_ s64 atom_compare_exchange(s64 volatile& x, s64 compare, s64 replace)
 {
-    return LinuxGccNs::InterlockedCompareExchange64(&x, replace, compare);
+    s64 ret = compare;
+    LinuxGccNs::__atomic_compare_exchange_strong(&x, &ret, replace) ? compare : ret;
 }
 
 _INLINE_ void* atom_compare_exchange(void* volatile& x, void* compare, void* replace)
 {
-    return LinuxGccNs::InterlockedCompareExchangePointer(&x, replace, compare);
+    void* ret = compare;
+    LinuxGccNs::__atomic_compare_exchange_strong(&x, &ret, replace) ? compare : ret;
 }
